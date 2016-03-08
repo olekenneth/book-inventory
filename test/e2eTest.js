bbcb4e56472cba2cfa7cec231729a190c04b07b3
Ole-Kenneth Bratholt <ok@vg.no>
2016-03-08 11:21:53 +0100

    Made sure the newly added object is stored in the mock database

--- test/e2eTest.js
+++ test/e2eTest.js
@@ -21,39 +21,17 @@ describe('/stock', function(){
     });
 
     it('GET', function(done){
+        let data = [{
+            isbn: '1234',
+            count: 5
+        }];
+
         request(app)
             .get('/stock')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
+            .expect(data)
             .end(done);
     });
-
-    it('POST and store', function(done){
-        let data = [{
-            isbn: '1234',
-            count: 5
-        },{
-            isbn: '12345',
-            count: 5
-        }];
-
-        request(app)
-            .post('/stock')
-            .send(data)
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(201)
-            .end(done);
-    });
-
-
-    // it('get an error when not sending isbn', function(done){
-    //     request(app)
-    //         .post('/stock')
-    //         .set('Accept', 'application/json')
-    //         .expect(JSON.stringify('ISBN is required'))
-    //         .expect(500)
-    //         .end(done);
-    // });
 });
