3b25450592627c047040c2e76996da254605cc93
Ole-Kenneth Bratholt <ok@vg.no>
2016-03-08 11:06:46 +0100

    Alot of changes

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import repo from './stock-repo';
 let app = init(repo());
 app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port: ', process.env.PORT || 3000);
-    console.log('ENVs:', process.env);
+    // console.log('ENVs:', process.env);
 });
