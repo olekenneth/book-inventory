b813a06214cd74beaab52151e9fc754460bd1f95
Ole-Kenneth Bratholt <ok@vg.no>
2016-03-08 09:50:26 +0100

    Change console log

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ import init from './app';
 import repo from './stock-repo';
 
 let app = init(repo());
-app.listen(process.env.PORT || 3000, () => console.log('Listening on port: ', process.env.PORT || 3000); console.log('ENVs:', process.env););
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening on port: ', process.env.PORT || 3000);
+    console.log('ENVs:', process.env);
+});
